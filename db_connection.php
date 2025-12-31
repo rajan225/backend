f0cbd25766ef958244cbd5f6579f80da9f4d0ae7
Rajan Giri <rajan.giri225@gmail.com>
2025-12-31 15:21:53 +0530

    db change db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,6 @@ $servername = "mysql-4426148-rajan-fd02.l.aivencloud.com";
 $username   = "avnadmin";
 $password   = "AVNS_h5mG7pN3S6PnLsDcl33";
 $dbname     = "learnapp";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
