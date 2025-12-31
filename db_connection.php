1eba140eecf9133a9f4386d6d976b741a30307c5
Rajan Giri <rajan.giri225@gmail.com>
2025-12-31 15:59:00 +0530

    port add db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -3,8 +3,9 @@ $servername = "mysql-4426148-rajan-fd02.l.aivencloud.com";
 $username   = "avnadmin";
 $password   = "AVNS_h5mG7pN3S6PnLsDcl33";
 $dbname     = "learnapp";
+$port="27759";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname,$port);
 
 // Check connection
 if ($conn->connect_error) {
